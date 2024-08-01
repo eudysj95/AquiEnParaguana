3c09dddd1613d5160e825c7d7e9845654b9af78d
Eudys Mora <eudysjmorag@gmail.com>
2024-07-31 21:44:47 -0400

    Obteniendo los datos del backend

--- src/components/DondeComer.jsx
+++ src/components/DondeComer.jsx
@@ -1,18 +1,37 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable no-unused-vars */
 import React from 'react';
 import maxburguer from "../images/maxburguer.jpg"
 import { Tarjeta } from '../marcos/Tarjeta';
+import { useEffect, useState } from 'react'
 
 export const DondeComer = () => {
+
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+
+    conseguirUsuarios();
+  })
+
+  const conseguirUsuarios = async() => {
+    const url = "http://localhost:3900/api/listar";
+    let peticion = await fetch(url, {
+      method: "GET"
+    });
+    let datos = await peticion.json();
+
+    if(datos.status === "succes"){
+      setUsers(datos.users);
+    }
+
+    
+  }
+
   return (
     <div className='p-8 grid grid-cols-4 gap-8'>
 
-      <Tarjeta name="Max Burguers" descripcion="Restaurante de comida rápida" img={maxburguer} direccion="Av Táchira · Frente de La Polar" tlf="04246358747" ></Tarjeta>
-      <Tarjeta name="Max Burguers" descripcion="Restaurante de comida rápida" img={maxburguer} direccion="Av Táchira · Frente de La Polar" tlf="04246358747" ></Tarjeta>
-      <Tarjeta name="Max Burguers" descripcion="Restaurante de comida rápida" img={maxburguer} direccion="Av Táchira · Frente de La Polar" tlf="04246358747" ></Tarjeta>
-      <Tarjeta name="Max Burguers" descripcion="Restaurante de comida rápida" img={maxburguer} direccion="Av Táchira · Frente de La Polar" tlf="04246358747" ></Tarjeta>
-      <Tarjeta name="Max Burguers" descripcion="Restaurante de comida rápida" img={maxburguer} direccion="Av Táchira · Frente de La Polar" tlf="04246358747" ></Tarjeta>
-      <Tarjeta name="Max Burguers" descripcion="Restaurante de comida rápida" img={maxburguer} direccion="Av Táchira · Frente de La Polar" tlf="04246358747" ></Tarjeta>
+      <Tarjeta users={users} ></Tarjeta>
       
 
     </div>
--- src/marcos/Tarjeta.jsx
+++ src/marcos/Tarjeta.jsx
@@ -1,19 +1,32 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable react/no-unknown-property */
 /* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
 import React from 'react'
 
-export const Tarjeta = ({name, descripcion, img, direccion, tlf}) => {
+
+export const Tarjeta = ({users}) => {
+
   return (
-    <div className='flex flex-col justify-between h-60 w-60 bg-[#00A3E8] p-2 text-center text-white rounded-md'>
-
-        <h2 className='text-xl font-bold'>{name}</h2>
-        <p>{descripcion}</p>
-        <img className='mx-auto rounded' src={img} alt="maxburguer" />
-        <p>{direccion}</p>
-        <p>{tlf}</p>
-        
-
-    </div>
+    users.length >= 1 ? (
+      users.map(user => {
+        return (
+          <div key={user._id} className='flex flex-col justify-between h-60 w-60 bg-[#00A3E8] p-2 text-center text-white rounded-md'>
+  
+          <h2 className='text-xl font-bold'>{user.nombre}</h2>
+          <p>{user.descripcion}</p>
+          {!user.imagen && <img className='mx-auto rounded' src="../images/default.png" alt="img" />}
+          {user.imagen && <img className='mx-auto rounded h-24' src={"http://localhost:3900/api/imagen/"+user.imagen} alt="img" />}
+          <p>{user.direccion}</p>
+          <p>{user.telefono}</p>
+          
+  
+      </div>
+        )
+      })
+    )
+    : <h2>No hay usuarios</h2>
+    
+    
   )
 }
